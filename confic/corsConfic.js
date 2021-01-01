db78690ab41cee382776617fec5253d576b08e81
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-02 00:08:25 +0600

    add whitelist ulr

--- confic/corsConfic.js
+++ confic/corsConfic.js
@@ -1,4 +1,8 @@
-var whitelist = ["http://localhost:3000", "https://your-moments.netlify.app"];
+var whitelist = [
+  "http://localhost:3000",
+  "https://your-moments.netlify.app",
+  "https://musing-sammet-263c2f.netlify.app"
+];
 
 var corsOptions = {
   origin: function (origin, callback) {
