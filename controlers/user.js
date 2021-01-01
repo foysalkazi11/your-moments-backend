d44d7d9076d8fd52c97aab45ce5960b2d7e701c6
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-01 11:33:52 +0600

    cookie setup

--- controlers/user.js
+++ controlers/user.js
@@ -10,9 +10,9 @@ module.exports.registerUser = async (req, res) => {
     if (newUser) {
       const token = signToken(newUser._id);
       res.cookie("access_token", token, {
-        httpOnly: true,
-        sameSite: true
-        // secure: true
+        // httpOnly: true,
+        sameSite: true,
+        secure: true
       });
       res.status(201).json({
         mesBody: "user added successfully",
