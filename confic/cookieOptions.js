c7d4eef3c975a52a3aa9712d4963535ecb304509
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-03 09:55:03 +0600

    change cookie options

--- confic/cookieOptions.js
+++ confic/cookieOptions.js
@@ -4,14 +4,14 @@ let cookieOptions = {
   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   // Set the cookie's HttpOnly flag to ensure the cookie is
   // not accessible through JS, making it immune to XSS attacks
-  // httpOnly: true,
+  httpOnly: false,
   sameSite: "none"
 };
 // In production, set the cookie's Secure flag
 // to ensure the cookie is only sent over HTTPS
-// if (process.env.NODE_ENV === "production") {
-//   cookieOptions.secure = true;
-// }
+if (process.env.NODE_ENV === "production") {
+  cookieOptions.secure = false;
+}
 
 module.exports = cookieOptions;
 
