5eafa2ea8fbed7bb0926b5474369053a0a87eac6
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-03 15:28:51 +0600

    add http res method

--- middleware/allowCrssDomain.js
+++ middleware/allowCrssDomain.js
@@ -2,7 +2,7 @@ const { whitelist } = require("../confic/corsConfic");
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://your-moments.netlify.app");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept,x-nf-request-id,Authorization"
