b470cd59f213b90201a272cfa02252d00c4b5d09
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-03 11:58:23 +0600

    add response header

--- middleware/allowCrssDomain.js
+++ middleware/allowCrssDomain.js
@@ -5,7 +5,7 @@ const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept,x-nf-request-id,Authorization"
   );
 
   next();
