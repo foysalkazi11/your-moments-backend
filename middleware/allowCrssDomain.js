4632cc663695b8f65f39827e8f3eb4db52126fe6
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-04 10:32:08 +0600

    change client url for testing

--- middleware/allowCrssDomain.js
+++ middleware/allowCrssDomain.js
@@ -1,6 +1,9 @@
 const { whitelist } = require("../confic/corsConfic");
 const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://your-moments.netlify.app");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://musing-sammet-263c2f.netlify.app"
+  );
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
   res.header(
