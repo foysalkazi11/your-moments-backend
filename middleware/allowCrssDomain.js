0fb608252186502176770d3251469c91b7fdb941
foysalkazi11 <foysalkazi11@gmail.com>
2021-01-12 12:32:21 +0600

    back clientSide netfity url

--- middleware/allowCrssDomain.js
+++ middleware/allowCrssDomain.js
@@ -1,6 +1,6 @@
 const { whitelist } = require("../confic/corsConfic");
 const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://your-moment.web.app");
+  res.header("Access-Control-Allow-Origin", "https://your-moments.netlify.app");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
   res.header(
